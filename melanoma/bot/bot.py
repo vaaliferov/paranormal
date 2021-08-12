19e7d311ae116ba817cc654583c4cf2f02c86c1f
vaaliferov <vaaliferov@gmail.com>
2021-08-12 18:37:19 +0300

    updated melanoma bot

--- melanoma/bot/bot.py
+++ melanoma/bot/bot.py
@@ -2,45 +2,46 @@
 
 import json
 import numpy as np
-from PIL import Image
+import onnxruntime
+from PIL import Image, ImageOps
 
 from telegram.ext import Updater
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
 
-import torch
-import torchvision
-import torch.nn as nn
-import torchvision.transforms.functional as F
-from torchvision.transforms import Compose, Resize
-from torchvision.transforms import ToTensor, Normalize
-
-from efficientnet_pytorch import EfficientNet
-
-def load_model(path):
-    model = EfficientNet.from_name('efficientnet-b6', num_classes=1)
-    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
-    return model.eval()
-
-def pad(x):
-    w, h = x.size; m = np.max([w, h])
+def pad(im):
+    w, h = im.size; m = np.max([w, h])
     hp, hpr = (m - w) // 2, (m - w) % 2
     vp, vpr = (m - h) // 2, (m - h) % 2
-    p = (hp + hpr, vp + vpr, hp, vp)
-    return F.pad(x, p, 0, 'constant')
+    return (hp + hpr, vp + vpr, hp, vp)
 
-def to_tensor(x):
-    x = ToTensor()(pad(x))
+def norm(x):
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
-    return Normalize(mean, std)(x)
+    return (x - mean) / std
+
+def load_image(path, size):
+    im = Image.open(path)
+    im.thumbnail((size, size), Image.ANTIALIAS)
+    im = ImageOps.expand(im, pad(im))
+    return np.array(im) / 255.
+
+def to_tensor(x):
+    x = np.float32(norm(x))
+    x = x.transpose(2,0,1)
+    return x.reshape((1,) + x.shape)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def load_model(path):
+    return onnxruntime.InferenceSession(path)
 
 def predict(model, path):
-    x = Image.open(path)
-    x.thumbnail((224,224), Image.ANTIALIAS)
-    x = to_tensor(x).unsqueeze(0)
-    y = model(x).detach()[0]
-    y = nn.Sigmoid()(y).item()
+    x = to_tensor(load_image(path, 224))
+    inps = {model.get_inputs()[0].name: x}
+    outs = model.run(None, inps)
+    y = sigmoid(outs[0])[0][0]
     return int(y > 0.5), y
 
 def handle_text(update, context):
@@ -52,7 +53,7 @@ def handle_photo(update, context):
     pred, prob = predict(model, 'in.jpg')
     update.message.reply_text(f'{pred} ({prob:.4f})')
 
-model = load_model('model.pt')
+model = load_model('model.onnx')
 opt = json.load(open('config.json','r'))
 updater = Updater(opt['bot_token'])
 dispatcher = updater.dispatcher
--- melanoma/bot/readme.md
+++ melanoma/bot/readme.md
@@ -17,7 +17,8 @@ chown <user> /opt/melanoma
 chmod 755 /opt/melanoma
 cp bot.py config.json /opt/melanoma
 cp melanoma_bot.service /etc/systemd/system
-gdown --id 1t74E3bt8bzW3ZDHqL4cV2nmlTSXQhmF7 -O /opt/melanoma/model.pt
+
+gdown --id 1KNeRnzxYF4X-DhvFfL_mrMDabWnTrPmg -O /opt/melanoma/model.onnx
 chmod +x /opt/melanoma/bot.py
 
 systemctl daemon-reload
@@ -44,13 +45,14 @@ chmod 755 /opt/melanoma
 cp bot.py config.json /opt/melanoma
 cp melanoma_bot.service /etc/systemd/system
 
+apt install python3-venv
 pip3 install virtualenv
 python3 -m venv /opt/melanoma/env
 source /opt/melanoma/env/bin/activate
 pip3 install -r requirements.txt
-source deactivate
+deactivate
 
-gdown --id 1t74E3bt8bzW3ZDHqL4cV2nmlTSXQhmF7 -O /opt/melanoma/model.pt
+gdown --id 1KNeRnzxYF4X-DhvFfL_mrMDabWnTrPmg -O /opt/melanoma/model.onnx
 chmod +x /opt/melanoma/bot.py
 
 systemctl daemon-reload
@@ -67,10 +69,10 @@ git clone https://github.com/vaaliferov/paranormal.git
 
 cd paranormal/melanoma/bot
 vim config.json (<token>)
-gdown --id 1t74E3bt8bzW3ZDHqL4cV2nmlTSXQhmF7 -O model.pt
+gdown --id 1KNeRnzxYF4X-DhvFfL_mrMDabWnTrPmg -O model.onnx
 
 docker build -t <user>/melanoma_bot .
-docker run --rm --user user --name melanoma_bot <user>/melanoma_bot
+docker run --rm --name melanoma_bot <user>/melanoma_bot
 ```
 
 https://t.me/paranormal_melanoma_bot
