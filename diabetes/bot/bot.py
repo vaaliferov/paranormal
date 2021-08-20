a5865e66d6f0ecf88c344d93a99e71de22f9ae38
vaaliferov <vaaliferov@gmail.com>
2021-08-20 12:40:10 +0300

    added multi-language support

--- diabetes/bot/bot.py
+++ diabetes/bot/bot.py
@@ -9,6 +9,30 @@ from telegram.ext import Filters
 from telegram.ext import MessageHandler
 
 
+l_yes = 'да'
+l_no = 'нет'
+l_male = 'м'
+l_female = 'ж'
+l_age = 'Укажите Ваш возраст?'
+l_gender = 'Ваш пол? (м / ж)'
+l_polyuria = 'Есть ли у Вас симптомы полиурии (увеличенное образование мочи)? (Да / Нет)'
+l_polydipsia = 'Испытываете ли Вы неутолимую жажду? (Да / Нет)'
+l_sudden_weight_loss = 'Наблюдается ли у Вас внезапная потеря веса? (Да / Нет)'
+l_weakness = 'Испытываете ли слабость? (Да / Нет)'
+l_polyphagia = 'Наблюдаете ли у себя повышенный аппетит? (Да / Нет)'
+l_genital_thrush = 'Генитальная молочница? (Да / Нет)'
+l_visual_blurring = 'Расплывчатость зрения? (Да / Нет)'
+l_itching = 'Зуд? (Да / Нет)'
+l_irritability = 'Раздражительность? (Да / Нет)'
+l_delayed_healing = 'Медленное заживление ран? (Да / Нет)'
+l_partial_paresis = 'Частичный парез (потеря мышечной силы)? (Да / Нет)'
+l_muscle_stiffness = 'Жесткость мышц? (Да / Нет)'
+l_alopecia = 'Усиленное выпадение волос? (Да / Нет)'
+l_obesity = 'Ожирение? (Да / Нет)'
+l_high_risk = 'Высокий риск наличия заболевания, рекомендуется пройти полное обследование'
+l_low_risk = 'Низкий риск наличия заболевания, нет необходимости в прохождении полного обследования'
+
+
 classes = [
     'negative', 
     'positive'
@@ -34,22 +58,22 @@ columns = [
 ]
 
 questions = [
-    ['Укажите Ваш возраст?', int, 0, 120, None],
-    ['Ваш пол? (м / ж)', str, None, None, ['м','ж']],
-    ['Есть ли у Вас симптомы полиурии (увеличенное образование мочи)? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Испытываете ли Вы неутолимую жажду?? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Наблюдается ли у Вас внезапная потеря веса? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Испытываете ли слабость? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Наблюдаете ли у себя повышенный аппетит? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Генитальная молочница? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Расплывчатость зрения? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Зуд? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Раздражительность? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Медленное заживление ран? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Частичный парез (потеря мышечной силы)? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Жесткость мышц? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Усиленное выпадение волос? (Да / Нет)', str, None, None, ['Да','Нет']],
-    ['Ожирение? (Да / Нет)', str, None, None, ['Да','Нет']]
+    [l_age, int, 0, 120, None],
+    [l_gender, str, None, None, [l_male,l_female]],
+    [l_polyuria, str, None, None, [l_yes,l_no]],
+    [l_polydipsia, str, None, None, [l_yes,l_no]],
+    [l_sudden_weight_loss, str, None, None, [l_yes,l_no]],
+    [l_weakness, str, None, None, [l_yes,l_no]],
+    [l_polyphagia, str, None, None, [l_yes,l_no]],
+    [l_genital_thrush, str, None, None, [l_yes,l_no]],
+    [l_visual_blurring, str, None, None, [l_yes,l_no]],
+    [l_itching, str, None, None, [l_yes,l_no]],
+    [l_irritability, str, None, None, [l_yes,l_no]],
+    [l_delayed_healing, str, None, None, [l_yes,l_no]],
+    [l_partial_paresis, str, None, None, [l_yes,l_no]],
+    [l_muscle_stiffness, str, None, None, [l_yes,l_no]],
+    [l_alopecia, str, None, None, [l_yes,l_no]],
+    [l_obesity, str, None, None, [l_yes,l_no]]
 ]
 
 answers = {}
@@ -88,11 +112,12 @@ def handle_text(update, context):
         
         else:
             data = pd.DataFrame([answers[chat_id]], columns=columns)
-            data.replace({'м':1,'ж':0}, inplace=True)
-            data.replace({'Да':1,'Нет':0}, inplace=True)
+            data.replace({l_male:1,l_female:0}, inplace=True)
+            data.replace({l_yes:1,l_no:0}, inplace=True)
             probas = model.predict_proba(data)[:,1]
             predicted = model.predict(data)
-            update.message.reply_text(f'{classes[predicted[0]]} ({probas[0]:.4f})')
+            msg = l_high_risk if predicted[0] == 1 else l_low_risk
+            update.message.reply_text(f'{msg} ({probas[0]:.4f})')
 
 
 model = pickle.load(open('ext_random_forest.pkl', 'rb'))
